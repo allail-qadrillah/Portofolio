708a5f64aa100f63d086dc3e02b884f2b71c373c
Muhammad Al lail Qadrillah <mallailqadrillah43@gmail.com>
2023-10-03 17:00:07 +0700

    change > intoductction

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,22 @@
 import Container from "@/components/elements/Container"
 import Home from "@/modules/home"
-
+import Link from "next/link"
+import { TbCoffee } from 'react-icons/tb'
 export default async function HomePage() {
   return (
     <>
       <Container data-aos="fade-up">
+      
+        <div className="flex justify-between items-center">
+          <div className="flex gap-2 font-bold text-4xl">
+            <h1> Hi, I&apos;m Alil</h1>
+            <div className="ml-1 animate-waving-hand">👋</div>
+          </div>
+          <Link href="/" target="_blank" aria-label="buy-me-cofey">
+            <TbCoffee size={25} />
+          </Link>
+        </div>
+
         <Home />
       </Container>
     </>
--- modules/home/components/Introduction.jsx
+++ modules/home/components/Introduction.jsx
@@ -4,15 +4,6 @@ import { TbCoffee } from 'react-icons/tb'
 export default function Introduction() {
   return (
     <section className="bg-cover bg-no-repeat space-y-2">
-      <div className="flex justify-between items-center">
-        <div className="flex gap-2 font-bold text-3xl sm:text-xl">
-          <h1> Hi, I&apos;m Alil</h1>
-          <div className="ml-1 animate-waving-hand">👋</div>
-        </div>
-        <Link href="/" target="_blank" aria-label="buy-me-cofey">
-          <TbCoffee size={25} />
-        </Link>
-      </div>
 
       <div className="mt-2 space-y-4">
         <ul className="flex flex-col gap-1 lg:flex-row lg:gap-8 ml-5 text-neutral-500 dark:text-neutral-400">
