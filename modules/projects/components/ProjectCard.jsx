23de4c85ed1e499ffc9a1b02973fc96a735387e7
Muhammad Al lail Qadrillah <mallailqadrillah43@gmail.com>
2023-10-15 16:47:01 +0700

    add > badge type of projects change > consume api notion projects

--- modules/projects/components/ProjectCard.jsx
+++ modules/projects/components/ProjectCard.jsx
@@ -1,9 +1,9 @@
-import Image from "next/image"
 import Link from "next/link"
 
-import { Card } from "@/components/ui/card"
-import { Badge } from "@/components/ui/badge"
 import ToolTips from "@/components/elements/Tooltip"
+import Image from '@/components/elements/Image';
+import { Badge } from "@/components/ui/badge"
+import { Card } from "@/components/ui/card"
 import { STACKS } from "@/constant/stack"
 import {
   AiFillPushpin as Pinned,
@@ -11,7 +11,9 @@ import {
 } from "react-icons/ai"
 
 export default function ProjectCard({
-  title, slug, description, image, stack, is_featured, is_public }) {
+  title, slug, description, image, stack, type, is_featured, is_public }) {
+
+  const trimmedContent = description.slice(0, 63) + (description.length > 70 ? '...' : '');
 
   return (
     <Link href={`/projects/${slug}`}>
@@ -23,22 +25,33 @@ export default function ProjectCard({
             </div>
           </div>
         )}
+
         <Image
           src={image}
-          width={600}
-          height={200}
           alt={title}
-          className="rounded-t-xl h-60 object-cover object-left"
+          width={800}
+          height={400}
+          rounded="rounded"
+          className="lg:hover:scale-105"
         />
-        <div className="p-5 space-y-2">
-          <div className="text-2xl text-neutral-700 dark:text-neutral-300 lg:hover:text-teal-800 dark:hover:text-teal-400 transition-all duration-300">
+
+        <div className="p-5">
+          <div className="text-2xl font-bold text-neutral-700 dark:text-neutral-300 lg:hover:text-teal-800 dark:hover:text-teal-400 transition-all duration-300">
             {title}
           </div>
-          <p className="text-lg text-neutral-700 dark:text-neutral-400 text-[15px] leading-relaxed">
-            {description}
+          <p className="text-sm mb-2 text-neutral-700 dark:text-neutral-400 text-[15px] leading-relaxed">
+            {trimmedContent}
           </p>
 
-          <div className="flex flex-wrap items-center gap-3">
+          <div className="flex flex-wrap items-center gap-2">
+            {type?.map((type, index) => (
+              <Badge className={`bg-${type.color}-200 text-neutral-700 dark:text-neutral-900 hover:bg-${type.color}/800`} key={index}>
+                {type.name}
+              </Badge>
+            ))}
+          </div>
+
+          <div className="flex mt-2 flex-wrap items-center gap-3">
             {stack?.map((stack, index) => (
               <div key={index} className="w-6">
                 <ToolTips title={stack}>{STACKS[stack]}</ToolTips>
--- modules/projects/components/ProjectDetail.jsx
+++ modules/projects/components/ProjectDetail.jsx
@@ -3,11 +3,13 @@ import NotionPage from "@/components/elements/NotionPage"
 import ToolTips from "@/components/elements/Tooltip";
 import Image from '@/components/elements/Image';
 import { STACKS } from "@/constant/stack"
+
+import { Badge } from "@/components/ui/badge"
+
 export default function ProjectDetail({ content }) {
-  console.log(content)
   return (
-    <>
-      <div className="flex flex-col lg:flex-row items-start lg:items-center sm:flex-row gap-5 justify-between">
+    <div className="space-y-4">
+      <div className="flex flex-col lg:flex-row sm:flex-row items-start lg:items-center  gap-5 justify-between">
         <div className="flex items-center flex-wrap gap-2">
           <span className="text-[15px] mb-1 text-neutral-700 dark:text-neutral-300">Tech Stack :</span>
           <div className="flex flex-wrap items-center gap-3">
@@ -18,18 +20,28 @@ export default function ProjectDetail({ content }) {
             ))}
           </div>
         </div>
-        <ProjectLink title={content.title} link_github={content.github}/>
 
+        <ProjectLink title={content.title} link_github={content.github} />
       </div>
+
+      <div className="flex flex-wrap items-center gap-2">
+        {content.type?.map((type, index) => (
+          <Badge className={`bg-${type.color}-200 text-neutral-700 dark:text-neutral-900 hover:bg-${type.color}/800`} key={index}>
+            {type.value}
+          </Badge>
+        ))}
+      </div>
+
       <Image
         src={content.image}
-        alt="profile image"
+        alt={content.title}
         width={800}
         height={400}
         rounded="rounded"
         className="lg:hover:scale-105"
       />
+
       <NotionPage recordMap={content.record_map} />
-    </>
+    </div>
   )
 }
