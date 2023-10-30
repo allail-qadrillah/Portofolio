8b6b98efb37f9d60a141acc29d23bdb361ba4c29
Muhammad Al lail Qadrillah <mallailqadrillah43@gmail.com>
2023-10-30 19:12:49 +0700

    justify text about

--- modules/about/components/Story.jsx
+++ modules/about/components/Story.jsx
@@ -3,7 +3,7 @@ import { ABOUT } from "@/constant/about"
 export default function Story() {
   return (
     <section 
-      className="text-lg space-y-4 leading-[1.8] md:leading-loose text-neutral-800 dark:text-neutral-300"
+      className="text-lg text-justify space-y-4 leading-[1.8] md:leading-loose text-neutral-800 dark:text-neutral-300"
     dangerouslySetInnerHTML={{ __html: ABOUT }}
     />
   )
