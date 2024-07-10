96c2620153b5ba17ce0b6779d2eb816ebbd1aa7e
mallailqadrillah <mallailqadrillah43@gmail.com>
2024-07-10 21:44:24 +0700

    set theme default to light

--- context/theme.jsx
+++ context/theme.jsx
@@ -5,7 +5,7 @@ import React, { ReactNode } from 'react';
 
 export default function ThemeProviderContext({ children }) {
   return (
-    <ThemeProvider attribute='class' defaultTheme="system">
+    <ThemeProvider attribute='class' defaultTheme="light">
       {children}
     </ThemeProvider>
   );
